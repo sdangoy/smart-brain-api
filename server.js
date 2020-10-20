324e465e573da02f0e82ee0e34a770383431fbad
Stefan Dangoy <stefan.dangoy@gmail.com>
2020-10-19 20:24:21 -0700

    281 - Created /profile/:id & /image endpoints and used Postman to test connections

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const database = {
 }
 
 app.get('/', (req, res) => {
-    res.send('this is working!');
+    res.send(database.users);
 })
 
 app.post('/signin', (req,res) => {
@@ -51,15 +51,35 @@ app.post('/register', (req,res) => {
     res.json(database.users[database.users.length-1]);
 })
 
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req,res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
 app.listen(3001, () => {
     console.log('app is running on port 3001');
 })
-
-/*
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId ==> GET = user
-/image --> PUT --> user
-
-*/
