3553984b124d494c730854817ddcf5fcae4cf735
Stefan Dangoy <stefan.dangoy@gmail.com>
2020-11-15 20:51:43 -0800

    320 - Security review and brough API usage to back-end to hide API key

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,20 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'b69603d13f6f4342802a094d88905f9a'
+  });
+
+const handleApiCall = () => (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (db) => (req, res) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +27,6 @@ const handleImage = (db) => (req, res) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
--- server.js
+++ server.js
@@ -37,12 +37,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
-
 app.post('/register', register.handleRegister(db, bcrypt))
-
 app.get('/profile/:id', profile.handleProfileGet(db))
-
 app.put('/image', image.handleImage(db))
+app.post('/imageurl', image.handleApiCall())
 
 app.listen(3001, () => {
     console.log('app is running on port 3001');
