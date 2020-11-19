984aba4c20ec52877e5818323602599cfb666baf
Stefan Dangoy <stefan.dangoy@gmail.com>
2020-11-18 23:39:46 -0800

    325 - Added fix to code: DEPTH_ZERO_SELF_SIGNED_CERT

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const signin = require('./controllers/signin.js');
 const profile = require('./controllers/profile.js');
 const image = require('./controllers/image.js');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+
 const db = knex({
         client: 'pg',
         connection: {
